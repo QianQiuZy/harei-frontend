e0357a01ebf8b2e02eb1e7e191fb8d1c293afd9d
QianQiuZy <308025580@qq.com>
2026-03-19 16:08:52 +0800

    fix about ifdian

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -48,23 +48,23 @@ const ABOUT_MARKDOWN = `# 花礼Harei的小空间
 
  - 斗虫源码：[VR_douchong](https://github.com/QianQiuZy/VR_douchong)
  - 斗虫衍生：[hihi粉丝站](https://dc.hihivr.top)
- - 斗虫QQbot插件：[nonebot-plugin](https://github.com/QianQiuZy/nonebot-plugin-vrpspdouchong)
+ - 斗虫QQbot插件：[nonebot2-plugin](https://github.com/QianQiuZy/nonebot-plugin-vrpspdouchong)
 
 ---
 
 ## 🗓️时间线
 
-- **2024-10-11**：项目初建立  
-- **2024-10-13**：正式上线  
-- **2026-01-09**：项目完全重写  
+- **2024-10-11**：项目初建立
+- **2024-10-13**：正式上线
+- **2026-01-09**：项目完全重写
 
 ---
 
 ## 👤关于作者
 
-- ✉️邮箱：qianqiuzy@qq.com  
-- 🔗Bilibili：[千秋紫莹](https://space.bilibili.com/351708822)  
-- 👋赞助支持：[爱发电](https://afdian.com/a/qianqiuzy)  
+- ✉️邮箱：qianqiuzy@qq.com
+- 🔗Bilibili：[千秋紫莹](https://space.bilibili.com/351708822)
+- 👋赞助支持：[爱发电](https://ifdian.net/a/qianqiuzy)
 `;
 
 export default function AboutPage() {
